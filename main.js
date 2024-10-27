dc3fde7bafc7cc884d05e8e1d1383d02c0ac2c5f
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-10-27 16:11:01 -0400

    order management system

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const placeOrderButton = document.getElementById('place-order');
 const orderSummary = document.getElementById('order-summary');
 
 
-// Function to Calculate total price when product or quantity changes
+// Task 3: Function to Calculate total price when product or quantity changes
 function updateTotalPrice() {
     const productPrice = parseFloat(productSelector.value);
     const quantity = parseInt(quantityInput.value);
@@ -14,6 +14,16 @@ function updateTotalPrice() {
     
     totalPriceElement.textContent = totalPrice.toFixed(2);
 }
-// add event listner for product selection
+// Task 2: add event listner for product selection
 productSelector.addEventListener('change', updateTotalPrice);
 quantityInput.addEventListener('input', updateTotalPrice); 
+
+
+// Task: 4 Handle order submission
+placeOrderButton.addEventListener('click', function() {
+    const selectedProduct = productSelector.options[productSelector.selectedIndex].text;
+    const quantity = quantityInput.value;
+    const totalPrice = totalPriceElement.textContent;
+    
+    orderSummary.textContent = `You ordered ${quantity} of ${selectedProduct}. Total price: $${totalPrice}`;
+});
