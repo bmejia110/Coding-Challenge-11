90a3f355b98835d1e400e277acfabf3383f80903
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-10-26 20:26:33 -0400

    Calculate and update total price dynamically based on product and quantity

---
+++ main.js
@@ -0,0 +1,19 @@
+//drop down menu
+const productSelector = document.getElementById('product-selector'); 
+const quantityInput = document.getElementById('quantity');
+const totalPriceElement = document.getElementById('total-price');
+const placeOrderButton = document.getElementById('place-order');
+const orderSummary = document.getElementById('order-summary');
+
+
+// Function to Calculate total price when product or quantity changes
+function updateTotalPrice() {
+    const productPrice = parseFloat(productSelector.value);
+    const quantity = parseInt(quantityInput.value);
+    const totalPrice = productPrice * quantity;
+    
+    totalPriceElement.textContent = totalPrice.toFixed(2);
+}
+// add event listner for product selection
+productSelector.addEventListener('change', updateTotalPrice);
+quantityInput.addEventListener('input', updateTotalPrice);
